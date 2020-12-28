812f5d259da203721188b38f4e8fb461131f9266
tslilpress <tslilpress@gmail.com>
2020-12-28 16:30:49 -0500

    Added smoothscroll and style to active links
    - using jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-const { JSDOM } = require( "jsdom" )
-const { window } = new JSDOM( "" )
-const $ = require( "jquery" )( window )
+// const { JSDOM } = require( "jsdom" )
+// const { window } = new JSDOM( "" )
+// const $ = require( "jquery" )( window )
 
 $(document).ready(function() {
 
@@ -10,6 +10,25 @@ $(document).ready(function() {
 
     scrollLink.click(function(event) {
         event.preventDefault()
+        $('body, html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500)
+    })
+
+    // active link class switch 
+
+    $(window).scroll(function() {
+        const scrollBarLocation = $(this).scrollTop()
         
+        scrollLink.each(function() {
+            console.log(scrollBarLocation)
+            const sectionOffset = $(this.hash).offset().top - 72
+            if(sectionOffset <= scrollBarLocation) {
+                $(this).parent().addClass('active')
+                $(this).parent().siblings().removeClass('active')
+            } else if (scrollBarLocation === 0) {
+                $(this).parent().siblings().removeClass('active')
+            }
+        })
     })
 })
